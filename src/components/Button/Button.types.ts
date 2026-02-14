534d4224cb80765a92d2f8e89b6a833e90ac4978
PedroHRFerreira <devpedrohr@gmail.com>
2026-02-14 13:50:01 -0300

    feat: :sparkles: tipando o botão

---
+++ src/components/Button/Button.types.ts
@@ -0,0 +1,12 @@
+export interface ButtonTypes {
+  children: React.ReactNode;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  variant?: "primary" | "secondary" | "ghost";
+  size?: "small" | "medium" | "large";
+  className?: string;
+  colorPrimary?: string;
+  colorSecondary?: string;
+  hoverEffect?: boolean;
+}
