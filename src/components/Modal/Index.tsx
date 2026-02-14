464f0caa866cbf3f6be71b4be043194ae7e78e59
PedroHRFerreira <devpedrohr@gmail.com>
2026-02-14 12:53:35 -0300

    feat: :sparkles: Adicionando os campos do login

--- src/components/Modal/Index.tsx
+++ src/components/Modal/Index.tsx
@@ -1,7 +1,12 @@
 import style from "./styles.module.scss";
 import AtomsIconSvg from "../IconSvg";
+import Input from "../Input/Index";
+import { useState } from "react";
 
 const Modal = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <aside className={style.backdrop}>
       <div className={style.modal}>
@@ -15,6 +20,18 @@ const Modal = () => {
             />
             {/* TODO: ação de cadastrar um novo login. */}
           </div>
+          <Input
+            label="Email"
+            type="email"
+            value={email}
+            onInput={(event) => setEmail(event)}
+          />
+          <Input
+            label="Senha"
+            type="password"
+            value={password}
+            onInput={(event) => setPassword(event)}
+          />
         </form>
       </div>
     </aside>
