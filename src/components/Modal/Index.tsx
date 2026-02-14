fed1835e7b92d0366f5644b546048dd1040c692a
PedroHRFerreira <devpedrohr@gmail.com>
2026-02-14 11:51:34 -0300

    feat: :sparkles: preparando os componentes para tela de login

--- src/components/Modal/Index.tsx
+++ src/components/Modal/Index.tsx
@@ -1,9 +1,22 @@
 import style from "./styles.module.scss";
+import AtomsIconSvg from "../IconSvg";
 
 const Modal = () => {
   return (
     <aside className={style.backdrop}>
-      <div className={style.modal}>nejndjnej</div>
+      <div className={style.modal}>
+        <form className={style.modal__form}>
+          <div className={style.modal__form__border}>
+            <AtomsIconSvg
+              name="register_login"
+              width="64px"
+              height="64px"
+              className={style.modal__form__border__icon}
+            />
+            {/* TODO: ação de cadastrar um novo login. */}
+          </div>
+        </form>
+      </div>
     </aside>
   );
 };
