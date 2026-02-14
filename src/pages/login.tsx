dfa453891737cb489153b687acdfd544491b0843
PedroHRFerreira <devpedrohr@gmail.com>
2026-02-14 11:06:23 -0300

    feat: :sparkles: paginada criada

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import LoginLayout from "@/layouts/Login";
+
+export default function Login() {
+  return (
+    <LoginLayout>
+      <article>
+        <h1>Tela de Login</h1>
+      </article>
+    </LoginLayout>
+  );
+}
