6bb2dd927bb95b626abc752c113bcff7d82a7d16
PedroHRFerreira <devpedrohr@gmail.com>
2026-02-14 14:09:18 -0300

    feat: :sparkles: criando button

--- src/components/Modal/Index.tsx
+++ src/components/Modal/Index.tsx
@@ -1,14 +1,33 @@
 import style from "./styles.module.scss";
 import AtomsIconSvg from "../IconSvg";
 import Input from "../Input/Index";
+import Button from "../Button";
 import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
 const Modal = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const validate = () => {
+    if (!email) {
+      toast.error("Email é obrigatório.");
+    }
+    if (!password) {
+      toast.error("Senha é obrigatória.");
+    }
+
+    return email && password;
+  };
+
+  const register = () => {
+    if (!validate()) return;
+    console.log("Registro com:", { email, password });
+  };
+
   return (
     <aside className={style.backdrop}>
+      <Toaster />
       <div className={style.modal}>
         <form className={style.modal__form}>
           <div className={style.modal__form__border}>
@@ -32,6 +51,15 @@ const Modal = () => {
             value={password}
             onInput={(event) => setPassword(event)}
           />
+          <Button
+            variant="rounded"
+            size="large"
+            colorPrimary="#ff3d7f"
+            colorSecondary="#ff1a6e"
+            onClick={register}
+          >
+            LOGIN
+          </Button>
         </form>
       </div>
     </aside>
