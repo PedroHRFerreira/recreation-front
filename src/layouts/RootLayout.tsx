1a3f4a3704f13f9553a28793d78b575b50c68290
PedroHRFerreira <devpedrohr@gmail.com>
2026-02-16 16:55:47 -0300

    feat: :zap: adicioando o sidebar

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,17 +1,22 @@
 import "@/assets/globals.scss";
+import styles from "./styles.module.scss";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Sidebar from "@/components/Sidebar";
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header />
-      <main>{children}</main>
-      <footer>
-        <p>Footer do Site</p>
-      </footer>
+      <div className={styles.layout__body}>
+        <Sidebar />
+        <main className={styles.layout__main}>{children}</main>
+      </div>
+      <Footer />
     </div>
   );
 }
